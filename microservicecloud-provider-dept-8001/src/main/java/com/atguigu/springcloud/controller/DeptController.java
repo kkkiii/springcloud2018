357187754687d9d5669b7aacb2f8099a441c15f5
liuhongshuo <2624886248@qq.com>
2019-10-31 17:54:16 +0800

    a

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.controller;
 import java.util.List;
  
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
  
 import com.atguigu.springcloud.entities.Dept;
 import com.atguigu.springcloud.service.DeptService;
- 
+
+
 @RestController
 public class DeptController
 {
   @Autowired
   private DeptService service;
+  @Autowired
+  private DiscoveryClient client ;
   
   @RequestMapping(value="/dept/add",method=RequestMethod.POST)
   public boolean add(@RequestBody Dept dept)
@@ -36,7 +41,19 @@ public class DeptController
    return service.list();
   }
   
-  
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery()
+	{
+		List<String> list = client.getServices();
+		System.out.println("**********" + list);
+
+		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance element : srvList) {
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+					+ element.getUri());
+		}
+		return this.client;
+	}
   
 }
  
