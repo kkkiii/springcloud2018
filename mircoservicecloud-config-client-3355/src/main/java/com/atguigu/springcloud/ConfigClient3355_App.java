c6a24bb6092ef6eeee6e5b875251187c87b43fa8
liuhongshuo <2624886248@qq.com>
2019-11-04 15:29:04 +0800

    a

---
+++ mircoservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClient3355_App.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ConfigClient3355_App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(ConfigClient3355_App.class, args);
+	}
+
+}
